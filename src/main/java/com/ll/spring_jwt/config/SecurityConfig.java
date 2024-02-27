d1695acff9cbe958ea6f06b613ff2f62e58f0577
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-02-27 11:40:29 +0900

    feat: SecurityConfig에서 UsernamePasswordAuthenticationFilter를 대체해서 LoginFilter 등록

--- src/main/java/com/ll/spring_jwt/config/SecurityConfig.java
+++ src/main/java/com/ll/spring_jwt/config/SecurityConfig.java
@@ -1,21 +1,36 @@
 package com.ll.spring_jwt.config;
 
+import com.ll.spring_jwt.jwt.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     //비밀번호를 해싱 암호화 하기 때문에 Bean으로 등록해줌
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    //생성자 방식으로 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    //AuthenticationManager 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -42,6 +57,10 @@ public class SecurityConfig {
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 );
+
+
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         //중요 - JWT 방식에서는 세션을 항상 STATELESS 상태로 관리함
         //세션 설정
