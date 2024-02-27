dc47fffe479e23b11e302f7b9c66de08e45bab24
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-02-27 11:39:48 +0900

    feat: UsernamePasswordAuthenticationFilter를 상속받은 LoginFilter를 생성

---
+++ src/main/java/com/ll/spring_jwt/jwt/LoginFilter.java
@@ -0,0 +1,47 @@
+package com.ll.spring_jwt.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    //Authentication를 진행하기 위한 attemptAuthentication을 override해줘야한다.
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        //클라이언트 요청에서 username, password 추출
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        //시큐리티에서 username, password를 검증하기 위해서는 token에 담아야함
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        //token에 담은 정보를 검증을 위해 AuthenticationManager에 전달
+        return authenticationManager.authenticate(token);
+    }
+
+    //authenticationManager에서 검증 성공
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+
+    //authenticationManager에서 검증 실패
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        super.unsuccessfulAuthentication(request, response, failed);
+    }
+}
